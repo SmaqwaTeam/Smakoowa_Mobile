7a86901fa3bc3d25ab239095892b05ab673b301c
Chris <kbalicki2014@gmail.com>
2023-04-07 17:56:42 +0200

    #7 Search bar add

--- smakowa/lib/main.dart
+++ smakowa/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:smakowa/pages/favorite_page.dart';
 import 'package:smakowa/pages/home_page.dart';
 import 'package:smakowa/pages/profile_page.dart';
 import 'package:smakowa/pages/settings_page.dart';
+import 'package:easy_search_bar/easy_search_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -61,11 +62,24 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: MySearchDelaegate(),
+              );
+            },
+            icon: Icon(
+              Icons.search,
+            ),
+          )
+        ],
       ),
       body: Center(
         child: body[_currentIndex],
       ),
-
+//NAV BAR
       bottomNavigationBar: BottomNavigationBar(
         unselectedItemColor: Colors.grey,
         selectedItemColor: Colors.orange,
@@ -94,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-
+//ADD BUTTON
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Add',
@@ -103,9 +117,133 @@ class _MyHomePageState extends State<MyHomePage> {
           color: Colors.white70,
         ),
       ),
+//DRAWER
       drawer: Drawer(
-        child: Text("boo"),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            child: Text(''),
+          ),
+          DrawerListTile(
+            title: 'Categories',
+            icon: Icons.category_outlined,
+            onPress: () {},
+          ),
+          // Divider(
+          //   color: Colors.grey,
+          // ),
+          DrawerListTile(
+            title: 'Tags',
+            icon: Icons.tag,
+            onPress: () {},
+          ),
+          DrawerListTile(
+              title: 'Notifications',
+              icon: Icons.notifications_none_sharp,
+              onPress: () {}),
+        ],
+      )), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class DrawerListTile extends StatelessWidget {
+  const DrawerListTile({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.onPress,
+  });
+  final String title;
+  final IconData icon;
+  final VoidCallback onPress;
+  //onPress not implement!
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        color: Colors.orange,
+        size: 25,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontSize: 18,
+        ),
+      ),
+      onTap: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+}
+
+//SEARCH BAR
+class MySearchDelaegate extends SearchDelegate {
+  List<String> suggestions = [
+    'chicken',
+    'rice',
+    'packcake',
+    'meat',
+  ];
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null); // close tab
+      },
+    );
+  }
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          if (query.isEmpty) {
+            close(context, null);
+          }
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestions = [
+      'chicken',
+      'rice',
+      'packcake',
+      'meat',
+    ];
+
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (context, index) {
+        final suggestion = suggestions[index];
+
+        return ListTile(
+          title: Text(suggestion),
+          onTap: () {
+            query = suggestion;
+            showResults(context);
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) => Center(
+        child: Text(
+          query,
+        ),
+      );
 }
