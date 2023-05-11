17a1b1a0c7bda6dc6e703e815baf59f30e64d125
Chris <kbalicki2014@gmail.com>
2023-05-11 19:19:02 +0200

    #2 profile list menu widget

---
+++ smakowa/lib/pages/widget/profile_list_menu.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ProfileListMenu extends StatelessWidget {
+  const ProfileListMenu({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.onPress,
+  });
+
+  final String title;
+  final IconData icon;
+  final VoidCallback onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onPress,
+      leading: Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: Colors.grey[200],
+        ),
+        child: Icon(
+          icon,
+          color: Colors.amber,
+        ),
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+}
